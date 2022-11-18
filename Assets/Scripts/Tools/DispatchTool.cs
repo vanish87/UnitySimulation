8ab4f12736d927366bcf489c3c239f07b3e97e7b
Yuan Li <vanish8.7@gmail.com>
2022-11-18 14:50:52 +0900

    add no group size dispath

--- Assets/Scripts/Tools/DispatchTool.cs
+++ Assets/Scripts/Tools/DispatchTool.cs
@@ -11,6 +11,18 @@ namespace Simulation
             var k = cs.FindKernel(kernel);
             DispatchTool.Dispatch(cs, k, dsize);
         }
+        public static void DispatchNoGroup(ComputeShader cs, string kernel, int3 dsize)
+        {
+            var k = cs.FindKernel(kernel);
+            DispatchTool.DispatchNoGroup(cs, k, dsize);
+        }
+        public static void DispatchNoGroup(ComputeShader cs, int k, int3 dsize)
+        {
+            cs.SetInt("_DispatchedX", dsize.x);
+            cs.SetInt("_DispatchedY", dsize.y);
+            cs.SetInt("_DispatchedZ", dsize.z);
+            cs.Dispatch(k, dsize.x, dsize.y, dsize.z);
+        }
         public static void Dispatch(ComputeShader cs, int k, int3 dsize)
         {
             uint threadNumX = 0; uint threadNumY = 0; uint threadNumZ = 0;
