fc068a94d96230191e11429f79b6586717353076
Yuan Li <vanish8.7@gmail.com>
2022-11-21 14:06:40 +0900

    update simulation step; remove init/deinit because it only needs run once

--- Assets/Scripts/Interface/SimulationStep.cs
+++ Assets/Scripts/Interface/SimulationStep.cs
@@ -2,23 +2,26 @@ namespace Simulation
 {
     public enum SimulationStep
     {
-        Init = 0,//: 0 ~ 1000
+        // Init = 0,//: 0 ~ 1000
         // DataInit = 100,
         // PluginInit = 500,
 
         Validate = 1000,
 
         // Simulation: 2000 ~ 10000
+
+        //Emitter/Boundary Update
         BeforeSimulation = 2000,
-        OnSetupConstants = 3000,
-        OnSetupBuffer = 4000,
+
+        //Update sorted grid hash etc.
+        PrepareData = 4000,
         OnSimulation = 5000,
         AfterSimulation = 6000,
         PostProcess = 7000,
 
         Render = 10000,
 
-        Deinit = 20000,// 20000 ~ 
+        // Deinit = 20000,// 20000 ~ 
         // PluginDeinit = 30000,
         // DataDeinit = 40000,
     }
